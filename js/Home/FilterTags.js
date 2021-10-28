fcc0640e0b5bae444e593b916a2a7bbb67a6f7c1
Tydis KD <82412416+KD-Kongo-Dervilon@users.noreply.github.com>
2021-10-28 15:53:26 +0200

    Modification de index.html et commentaire dans le FiltersTags Js
    Modification du fichier html et commentaire  dans le FiltersTags js
    Modification dans le Homepage/header.css

--- js/Home/FilterTags.js
+++ js/Home/FilterTags.js
@@ -14,6 +14,7 @@ export default class Filter {
         filtres.addEventListener('click', e => {
             let classValue = e.target.classList.value;
 
+            // returns the first index for which a given element is found in an array
             if (-1 === classValue.indexOf('actived')) {
                 e.target.classList.add('actived')
             } else {
@@ -30,7 +31,10 @@ export default class Filter {
         let currentFilters = document.querySelectorAll('ul li.actived');
         let filterSelected = [];
 
+        // allows you to execute a given function on each element of the array.
         currentFilters.forEach(function (currentFilter) {
+            //adds one or more elements to the end of an array and returns the new size of the array.
+            //returns the value of a given attribute of the specified element
             filterSelected.push(currentFilter.getAttribute("data-filter"));
         });
 
@@ -42,6 +46,7 @@ export default class Filter {
         let filters = this.getActiveFilters();
         let classValue = article.classList.value;
         let classes = classValue.split(' ');
+        
         let intersection = filters.filter(
             x => classes.includes(x)
         );
